39be63ee7ca101c0f182a546967fac9f893f4e17
Jost1001 <jost1001@stud.ek.dk>
2026-04-23 13:19:50 +0200

    stole animation virker!!!

--- js/darkmode.js
+++ js/darkmode.js
@@ -2,13 +2,12 @@ const html = document.querySelector("html");
 const btn = document.querySelector("#toggle-btn");
 
 function toggleTheme() {
-  console.log("toggleTheme");
   html.classList.toggle("dark");
 
-  if (btn.textContent == "Dark") {
-    btn.textContent = "Light";
+  if (html.classList.contains("dark")) {
+    btn.innerHTML = '<img src="imgs/light_mode_sun.png" alt="Light">';
   } else {
-    btn.textContent = "Dark";
+    btn.innerHTML = '<img src="imgs/dark_mode_moon.png" alt="Dark">';
   }
 }
 btn.addEventListener("click", toggleTheme);
